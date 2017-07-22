ad07dd1cf81f4ea14bf443fc03f04f8c98259944
pdhoward <patrick.howard@hotmail.com>
2017-07-22 17:09:58 -0400

    draft update - backup code

--- src/components/App.js
+++ src/components/App.js
@@ -3,16 +3,68 @@
 /////////////////    Container for the Books App        //////////////////
 ////////////////////////////////////////////////////////////////////////
 
-import React    from "react";
-import Navbar   from "./common/Navbar";
-import Footer   from "./common/Footer";
+import React, { Component }   from "react";
+import Navbar                 from "./common/Navbar";
+import Footer                 from "./common/Footer";
 
-const App = props => (
-  <div>
-    <Navbar />
-    {props.children}
-    <Footer />
-  </div>
-);
+
+// listhelves need to display -- siomcilar to contacts app list contacts
+// search needs to do the dynamic search -- as embedded in contacts/listcontacts
+
+class App extends Component {
+  state = {
+    shelves: [ ]
+  }
+
+  removeBook = (shelves) => {
+    this.setState( (state) => ({
+      // todo
+      //contacts: state.contacts.filter((c) => c.id !== contact.id )
+    }) )
+    //ContactsAPI.remove(contact)
+  }
+
+  moveBook(shelves) {
+    // todo - move to 1 of 3 shelves currently reading, want to read, read
+
+  }
+  favoriteBook(shelves) {
+    // todo - star the book
+
+  }
+
+  componentDidMount() {
+    // retrieve all books -- from backend server?
+  //  ContactsAPI.getAll().then((contacts) => {
+  //    this.setState({ contacts })
+  //  })
+  }
+  render() {
+    return (
+      <div className = 'app'>
+        <Navbar />
+
+        <Route exact path ="/" render={() => (
+          <ListBooks
+            onDeleteBook = { this.removeBook }
+            shelves={this.state.shelves}
+            />
+          )} />
+
+        <Route exact path ="/search" render={({history}) => (
+          <SearchBooks
+            onCreateContact={ (contact) => {
+              this.createContact(contact)
+              history.push('/')
+            }}
+            />
+          )} />
+
+       <Footer />
+
+       </div>
+    );
+  }
+}
 
 export default App;

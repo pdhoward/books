b5bd0334041e4116bbfd4e0c2d40da81707a3ba1
pdhoward <patrick.howard@hotmail.com>
2017-07-24 15:33:58 -0400

    created functional component for listing books on a shelf

---
+++ src/components/common/DisplayShelf.js
@@ -0,0 +1,43 @@
+
+//////////////////////////////////////////////////////////////////////////
+/////////////////  Stateless Functional Component to List ///////////////
+/////////////////       items on the prop                 ///////////////
+////////////////////////////////////////////////////////////////////////
+
+import React       from 'react'
+
+const DisplayShelf = (prop) => {
+  let showingBooks = prop.showingBooks
+  return (
+
+<ol className="books-grid">
+  {showingBooks.map((book) => (
+
+  <li key={book.id}>
+    <div className="book">
+      <div className="book-top">
+        <div className="book-cover" style={{ width: 128,
+                                             height: 193,
+                                             backgroundImage: `url(${book.imageLinks.thumbnail})` }}>
+        </div>
+
+        <div className="book-shelf-changer">
+          <select>
+            <option value="none" disabled>Move to...</option>
+            <option value="currentlyReading">Currently Reading</option>
+            <option value="wantToRead">Want to Read</option>
+            <option value="read">Read</option>
+            <option value="none">None</option>
+          </select>
+        </div>
+
+      </div>
+      <div className="book-title">{book.title}</div>
+      <div className="book-authors">{book.authors[0]}</div>
+    </div>
+  </li>
+  ))}
+</ol>
+)}
+
+export default DisplayShelf
--- src/components/common/ListBooks_BK.js
+++ src/components/common/ListBooks_BK.js
@@ -9,7 +9,6 @@ import { Link }               from 'react-router-dom'
 import PropTypes              from 'prop-types'
 import escapeRegExp           from 'escape-string-regexp'
 import sortBy                 from 'sort-by'
-import DisplayShelf           from './DisplayShelf'
 
 class ListBooks extends Component {
 
@@ -52,6 +51,8 @@ class ListBooks extends Component {
 
     return (
       <div className='list-books'>
+        {/*<div className='list-books-top'> */}
+          {/* <div className="search-books"> */}
            <div className="search-books-bar">
               <input
                 className='search-books'
@@ -61,10 +62,37 @@ class ListBooks extends Component {
                 onChange={ (event) => this.updateQuery(event.target.value)}
               />
            </div>
+        {/*  </div> */}
+      {/*  </div> */}
 
-      {/* Currently Reading */}
-          <DisplayShelf showingBooks={showingBooks} />
+      <ol className="books-grid">
+        {showingBooks.map((book) => (
 
+        <li key={book.id}>
+          <div className="book">
+            <div className="book-top">
+              <div className="book-cover" style={{ width: 128,
+                                                   height: 193,
+                                                   backgroundImage: `url(${book.imageLinks.thumbnail})` }}>
+              </div>
+
+              <div className="book-shelf-changer">
+                <select>
+                  <option value="none" disabled>Move to...</option>
+                  <option value="currentlyReading">Currently Reading</option>
+                  <option value="wantToRead">Want to Read</option>
+                  <option value="read">Read</option>
+                  <option value="none">None</option>
+                </select>
+              </div>
+
+            </div>
+            <div className="book-title">{book.title}</div>
+            <div className="book-authors">{book.authors[0]}</div>
+          </div>
+        </li>
+        ))}
+      </ol>
 
     </div>
     )

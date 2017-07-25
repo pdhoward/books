8bea715411ce6aeed414f1badff953d370b21a68
pdhoward <patrick.howard@hotmail.com>
2017-07-25 18:15:52 -0400

    trying to fix cursor

--- src/components/common/DisplayShelf.js
+++ src/components/common/DisplayShelf.js
@@ -16,42 +16,44 @@ const DisplayShelf = (prop) => {
       id: event.target.name
     }
     let shelf=event.target.value
+    console.log(">>>DISPLAYSHELF<<<<<")
+    console.log({book: book})
+    console.log({shelf: shelf})
+
     selectedOption(book, shelf)
   }
 
   return (
 
-<ol className="books-grid">
+    <ol className="books-grid">
 
-  {showingBooks.map((book) => (
+      {showingBooks.map((book) => (
 
-  <li key={book.id} >
-    <div className="book">
-      <div className="book-top">
-        <div className="book-cover" style={{ width: 128,
+        <li key={book.id} >
+          <div className="book">
+            <div className="book-top">
+              <div className="book-cover" style={{ width: 128,
                                              height: 193,
                                              backgroundImage: `url(${book.imageLinks.thumbnail})` }}>
+              </div>
+
+              <div className="book-shelf-changer">
+                <select id="readlist" name={book.id} onChange={makeChoice} >
+                  <option value="" disabled>Move to...</option>
+                  <option value="currentlyReading">Currently Reading</option>
+                  <option value="wantToRead">Want to Read</option>
+                  <option value="read">Read</option>
+                  <option value="none">None</option>
+                </select>
+             </div>
+
+          </div>
+            <div className="book-title">{book.title}</div>
+            <div className="book-authors">{book.authors[0]}</div>
         </div>
-
-        <div className="book-shelf-changer">
-
-          <select id="readlist" name={book.id} onChange={makeChoice} >
-              <option value="none" disabled>Move to...</option>
-              <option value="currentlyReading">Currently Reading</option>
-              <option value="wantToRead">Want to Read</option>
-              <option value="read">Read</option>
-              <option value="none">None</option>
-            </select>
-
-        </div>
-
-      </div>
-      <div className="book-title">{book.title}</div>
-      <div className="book-authors">{book.authors[0]}</div>
-    </div>
-  </li>
-  ))}
-</ol>
+      </li>
+     ))}
+   </ol>
 )}
 
 export default DisplayShelf
